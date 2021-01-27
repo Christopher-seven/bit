898584d94b0964c32bdfcea41d6b2d825bc0dfd7
Christopher-seven <1327109885@qq.com>
2021-01-27 23:10:48 +0800

    homework

---
+++ homework/homework/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[7] = { 0 };
+//	int i = 0;
+//	int sum = 0;
+//	int get_max = arr[0];
+//	int get_min = arr[0];
+//	for (i = 0; i<7; i++)
+//	{
+//		scanf("%d", &arr[i]);
+//		sum += arr[i];
+//	}
+//	for (i = 1; i<7; i++)
+//	{
+//		if (get_max<arr[i])
+//			get_max = arr[i];
+//		if (get_min>arr[i])
+//			get_min = arr[i];
+//	}
+//	sum = sum - get_max - get_min;
+//
+//	float score = 0.0;
+//	score = sum / 5.0;
+//	printf("%.2f\n", score);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 3; i <= 100; i += 3)
+//	{
+//		printf("%d ", i);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	scanf("%d%d", &i, &j);
+	while (1)
+	{
+
+	}
+}
